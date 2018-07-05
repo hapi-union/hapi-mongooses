088fb2660c6d68db1ac9ee9ab149fcb6ad9dde17
gcw <wangjinqiang@gongchang.com>
2018-07-05 14:57:08 +0800

    实现核心功能

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # hapi-mongooses
-hapi多mongodb连接解决方案
+一款用于hapi中多mongoose连接的hapi插件
+
+## 参数配置
+```
+server.register(
+  require('hapi-mongooses'),
+  {
+    default: `mongodb://username:${encodeURIComponent('password')}@hostname:port/defaultDb`,
+    other: `mongodb://username:${encodeURIComponent('password')}@hostname:port/otherDb`,
+  }
+)
+```
+
+## 依赖使用
+```
+server.dependency('hapi-mongooses', () => {
+  const { mongoose, connections, ObjectId, connection, Schema } = server.plugins['hapi-mongooses']
+  // const defaultConnection = connections['default']
+  const defaultConnection = connection
+  const otherConnection = connections['other']
+})
+```
+
+## 其他说明
+* ObjectId === mongoose.Types.ObjectId
---
+++ index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+exports.plugin = {
+  pkg: require('./package.json'),
+  register: async function (server, options) {
+    const connections = {}
+    Object.keys(options).forEach(key => {
+      const item = options[key]
+      const args = typeof item === 'string' ? [item] : item
+      const connection = mongoose.createConnection(args[0], args[1])
+      connections[key] = connection
+    })
+    const { Schema } = mongoose
+    const { ObjectId } = mongoose.Types
+    server.expose('Schema', Schema)
+    server.expose('ObjectId', ObjectId)
+    server.expose('mongoose', mongoose)
+    server.expose('connections', connections)
+    server.expose('connection', connections['default'])
+  }
+}
