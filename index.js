881abd23ecdc206b7b951351431f8980a52da2fb
gcw <wangjinqiang@gongchang.com>
2018-08-21 17:26:05 +0800

    增加插件接口

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ exports.plugin = {
       connections[key] = connection
     })
     const { Schema } = mongoose
-    const { ObjectId } = mongoose.Types
-    server.expose('Schema', Schema)
-    server.expose('ObjectId', ObjectId)
     server.expose('mongoose', mongoose)
+    server.expose('Schema', Schema)
+    // TODO delete expose of ObjectId
+    server.expose('ObjectId', mongoose.Types.ObjectId)
+    server.expose('ObjectIdType', Schema.Types.ObjectId)
+    server.expose('translateId', mongoose.Types.ObjectId)
     server.expose('connections', connections)
     server.expose('connection', connections['default'])
   }
